e7306da3d83d43bbdb2de99df7fde681a0d3176b
Chao-Wei, Peng <alanwei1202@gmail.com>
2019-07-23 10:35:15 +0800

    del sth

--- read.py
+++ read.py
@@ -31,24 +31,4 @@ for row in rows:
         
 db_connection.close()
 
-def random_plots():
-  xs = []
-  ys = []
-  
-  for i in range(20):
-    x = i
-    y = np.random.randint(10)
-    
-    xs.append(x)
-    ys.append(y)
-  
-  return xs, ys
 
-fig = plt.figure()
-ax2 = plt.subplot2grid((5, 2), (1, 0), rowspan=3, colspan=2)
-
-x, y = random_plots()
-ax2.plot(x, y)
-
-plt.tight_layout()
-plt.show()
